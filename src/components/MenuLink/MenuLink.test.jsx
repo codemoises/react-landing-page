aed6eec8d27f36f28f6e5fe36d7506ea5342f6f6
mwises <88902469+mwises@users.noreply.github.com>
2023-09-21 14:42:58 -0300

    Componente MenuLink com testes

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { MenuLink } from '.';
+
+describe('<MenuLink />', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link='https://localhost'>Children</MenuLink>);
+    expect(screen.getByRole('link', { name:'Children' })).toHaveAttribute('targe', '_self');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(<MenuLink link='https://localhost' newTab={true}>Children</MenuLink>);
+    expect(screen.getByRole('link', { name:'Children' })).toHaveAttribute('targe', '_blank');
+  });
+
+  it('should render open in a new tab', () => {
+    renderTheme(<MenuLink link='https://localhost' newTab={false}>Children</MenuLink>);
+    expect(screen.getByRole('link', { name:'Children' })).toMatchInlineSnapshot();
+  });
+});
