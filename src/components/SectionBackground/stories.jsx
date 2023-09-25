40d535983b98dec00fd141f9db372ea4b16f9695
mwises <88902469+mwises@users.noreply.github.com>
2023-09-25 11:03:15 -0300

    componentes: SectionBackground e SectionContainer

---
+++ src/components/SectionBackground/stories.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { SectionBackground } from '.';
+import { SectionContainer } from '../SectionContainer';
+
+export default {
+  title: 'SectionBackground',
+  component: SectionBackground,
+  args: {
+    children: (
+      <div>
+        <h1>SectionBackground</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque doloribus debitis minima nam beatae alias
+          eius, mollitia asperiores animi dolorem esse, aperiam nesciunt praesentium. Labore vel optio quaerat
+          aspernatur expedita!
+        </p>
+      </div>
+    ),
+  },
+  argTypes: {
+    children: { type: '' },
+  },
+};
---
+++ src/components/SectionContainer/index.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import P from 'prop-types';
+import * as Styled from './styles';
+import { SectionBackground } from '../SectionBackground';
+
+export const SectionContainer = ({ children, background = false }) => {
+  return (
+    <Styled.Container background={background}>
+        <SectionBackground>{children}</SectionBackground>
+    </Styled.Container>
+  );
+};
+
+SectionContainer.propTypes = {
+  children: P.node.isRequired,
+  background: P.bool,
+};
