86d60b5a304d6370acd6325f3f6fc7c2ecc83ad7
mwises <88902469+mwises@users.noreply.github.com>
2023-09-26 15:29:41 -0300

    testes SectionBackground e SectionContainer

--- src/components/SectionBackground/SectionBackground.test.jsx
+++ src/components/SectionBackground/SectionBackground.test.jsx
@@ -4,12 +4,23 @@ import { renderTheme } from '../../styles/render-theme';
 import { SectionBackground } from '.';
 
 describe('<SectionBackground />', () => {
-  it('should render with', () => {
-    renderTheme(
-        <SectionBackground background={true}>
-            <h1>Children</h1>
-        </SectionBackground>
+  it('should render with background dark', () => {
+    const { container } = renderTheme(
+      <SectionBackground background={true}>
+        <h1>Children</h1>
+      </SectionBackground>,
     );
     expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render with background light', () => {
+    const { container } = renderTheme(
+      <SectionBackground>
+        <h1>Children</h1>
+      </SectionBackground>,
+    );
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
   });
 });
