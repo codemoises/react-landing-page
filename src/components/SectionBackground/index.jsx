fba0d705d1800973fdb39a3bf3983b55d8ab85c1
mwises <88902469+mwises@users.noreply.github.com>
2023-09-26 15:52:14 -0300

    Componente SectionBackground

--- src/components/SectionBackground/index.jsx
+++ src/components/SectionBackground/index.jsx
@@ -3,9 +3,13 @@ import P from 'prop-types';
 import { SectionContainer } from '../SectionContainer';
 import * as Styled from './styles';
 
-export const SectionBackground = ({ children, background = false }) => {
+export const SectionBackground = ({
+  children,
+  background = false,
+  sectionId = '',
+}) => {
   return (
-    <Styled.Container background={background}>
+    <Styled.Container background={background} id={sectionId}>
       <SectionContainer>{children}</SectionContainer>
     </Styled.Container>
   );
@@ -14,4 +18,5 @@ export const SectionBackground = ({ children, background = false }) => {
 SectionBackground.propTypes = {
   children: P.node.isRequired,
   background: P.bool,
+  sectionId: P.string,
 };
