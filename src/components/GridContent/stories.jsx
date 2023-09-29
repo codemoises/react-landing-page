264ec98eef1276117dcfebcc2c3d94cb7645b708
mwises <88902469+mwises@users.noreply.github.com>
2023-09-29 13:38:54 -0300

    componente GridContent

---
+++ src/components/GridContent/stories.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { GridContent } from '.';
+import mock from './mock';
+
+export default {
+  title: 'GridContent',
+  component: GridContent,
+  args: mock,
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <GridContent {...args} />
+    </div>
+  );
+};
