8075aa777a93208d10e2116cc985fe3b0e7d904a
mwises <88902469+mwises@users.noreply.github.com>
2023-10-02 11:50:08 -0300

    Componente GridImage com testes

---
+++ src/components/GridImage/GridImage.test.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable prettier/prettier */
+import { renderTheme } from '../../styles/render-theme';
+import { GridImage } from '.';
+import mock from './mock';
+import React from 'react';
+
+describe('<GridImage />', () => {
+  it('should render with background', () => {
+    const { container } = renderTheme(<GridImage {...mock} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render without background', () => {
+    const { container } = renderTheme(
+      <GridImage {...mock} background={undefined} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
