4a75930e9790b15f681369ccde868226c75f1dd3
mwises <88902469+mwises@users.noreply.github.com>
2023-10-02 15:11:35 -0300

    Componente GoTop e testes

---
+++ src/components/GoTop/GoTop.test.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { screen } from '@testing-library/react';
+import { renderTheme } from '../../styles/render-theme';
+import { GoTop } from '.';
+
+describe('<GoTop />', () => {
+  it('should render a go to top button', () => {
+    const { container } = renderTheme(<GoTop />);
+    expect(screen.getByRole('link', { name: 'Go to top' })).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: 'Go to top' })).toHaveAttribute(
+        'href',
+        '#',
+    );
+    expect(container).toMatchInlineSnapshot();
+  });
+});
---
+++ src/components/GoTop/stories.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { GoTop } from '.';
+
+export default {
+  title: 'GoTop',
+  component: GoTop,
+  args: {
+    children: 'GoTop',
+  },
+  argTypes: {
+    children: { type: 'string' },
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <GoTop {...args} />
+    </div>
+  );
+};
