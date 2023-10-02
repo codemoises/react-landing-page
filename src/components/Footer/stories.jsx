c8d3f08b51dde73a8aded7e30ce8841b421fb156
mwises <88902469+mwises@users.noreply.github.com>
2023-10-02 14:36:36 -0300

    componente Footer com testes

---
+++ src/components/Footer/stories.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable prettier/prettier */
+import { Footer } from '.';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+  args: {
+    footerHtml: `<p><a href="https://bio-tau.vercel.app/">Feito por Moises Cruz Braga</a></p>`,
+  },
+};
+
+export const Template = (args) => {
+  return (
+    <div>
+      <Footer {...args} />
+    </div>
+  );
+};
