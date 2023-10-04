ae54f1b6896135a6c43a9b67189808bfcda9230b
mwises <88902469+mwises@users.noreply.github.com>
2023-10-04 15:44:39 -0300

    Mapeando SectionTwoColumn com testes

--- src/api/map-data.js
+++ src/api/map-data.js
@@ -1,4 +1,7 @@
 /* eslint-disable prettier/prettier */
+import { mapSections } from './map-sections';
+import { mapMenu } from './map-menu';
+
 export const mapData = (pagesData = [{}]) => {
   return pagesData.map((data) => {
     const {
@@ -13,8 +16,8 @@ export const mapData = (pagesData = [{}]) => {
       footerHtml,
       slug,
       title,
-      sections,
-      menu,
+      sections: mapSections(sections),
+      menu: mapMenu(menu),
     };
   });
 };
