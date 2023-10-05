23adca57ea22a52e783863f21d1042c7524d8780
mwises <88902469+mwises@users.noreply.github.com>
2023-10-05 17:53:30 -0300

    Limpando testes

--- src/api/map-sections.test.js
+++ src/api/map-sections.test.js
@@ -55,42 +55,15 @@ describe('map-sections', () => {
   it('should map section two columns', () => {
     const data = mapSectionTwoColumns({
       __component: 'section.section-two-columns',
-      _id: '602fdf2d540c00269e056178',
       title: 'title',
       description: 'abc',
       metadata: {
         background: true,
-        _id: '602fdf2d540c00269e05617b',
-        name: 'contact',
         section_id: 'contact',
-        __v: 0,
-        id: '602fdf2d540c00269e05617b',
       },
-      __v: 1,
       image: {
-        _id: '602fdc2b540c00269e05616a',
-        name: 'javascript.svg',
-        alternativeText: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-        caption: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-        hash: 'javascript_b57bf48cda',
-        ext: '.svg',
-        mime: 'image/svg+xml',
-        size: 30.31,
         url: 'a.svg',
-        provider_metadata: {
-          public_id: 'javascript_b57bf48cda',
-          resource_type: 'image',
-        },
-        provider: 'cloudinary',
-        width: 1030,
-        height: 730,
-        related: ['602fdf2d540c00269e056178', '602fdf2d540c00269e056172'],
-        createdAt: '2021-02-19T15:41:31.808Z',
-        updatedAt: '2021-02-19T15:54:22.590Z',
-        __v: 0,
-        id: '602fdc2b540c00269e05616a',
       },
-      id: '602fdf2d540c00269e056178',
     });
     expect(data.background).toBe(true);
     expect(data.component).toBe('section.section-two-columns');
@@ -112,42 +85,12 @@ describe('map-sections', () => {
   it('should map section content', () => {
     const data = mapSectionContent({
       __component: 'section.section-content',
-      _id: '602fdf2d540c00269e056178',
-      title: 'title',
+      title: 'Pricing',
       description: 'abc',
       metadata: {
-        background: true,
-        _id: '602fdf2d540c00269e05617b',
-        name: 'contact',
-        section_id: 'contact',
-        __v: 0,
-        id: '602fdf2d540c00269e05617b',
+        background: false,
+        section_id: 'pricing',
       },
-      __v: 1,
-      image: {
-        _id: '602fdc2b540c00269e05616a',
-        name: 'javascript.svg',
-        alternativeText: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-        caption: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-        hash: 'javascript_b57bf48cda',
-        ext: '.svg',
-        mime: 'image/svg+xml',
-        size: 30.31,
-        url: 'a.svg',
-        provider_metadata: {
-          public_id: 'javascript_b57bf48cda',
-          resource_type: 'image',
-        },
-        provider: 'cloudinary',
-        width: 1030,
-        height: 730,
-        related: ['602fdf2d540c00269e056178', '602fdf2d540c00269e056172'],
-        createdAt: '2021-02-19T15:41:31.808Z',
-        updatedAt: '2021-02-19T15:54:22.590Z',
-        __v: 0,
-        id: '602fdc2b540c00269e05616a',
-      },
-      id: '602fdf2d540c00269e056178',
     });
     expect(data.background).toBe(false);
     expect(data.component).toBe('section.section-content');
@@ -159,45 +102,24 @@ describe('map-sections', () => {
   it('should map grid text with data', () => {
     const data = mapTextGrid({
       __component: 'section.section-grid',
-      _id: '602fdf2d540c00269e056174',
       description: 'abc',
       title: 'My Grid',
       text_grid: [
         {
-          _id: '602fdf2d540c00269e05617c',
           title: 'Teste 1',
           description: 'Coisa',
-          __v: 0,
-          id: '602fdf2d540c00269e05617c',
         },
         {
-          _id: '602fdf2d540c00269e05617d',
           title: 'Teste 2',
           description:
-            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
-          __v: 0,
-          id: '602fdf2d540c00269e05617d',
-        },
-        {
-          _id: '602fdf2d540c00269e05617e',
-          title: 'Teste 3',
-          description:
-            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis cum delectus molestias. Atque doloribus nobis laudantium esse ut, non commodi maxime distinctio veritatis unde, reprehenderit minus ad dolores provident maiores.',
-          __v: 0,
-          id: '602fdf2d540c00269e05617e',
+            'abc',
         },
       ],
       image_grid: [],
       metadata: {
         background: true,
-        _id: '602fdf2e540c00269e056199',
-        name: 'grid-one',
         section_id: 'grid-one',
-        __v: 0,
-        id: '602fdf2e540c00269e056199',
       },
-      __v: 2,
-      id: '602fdf2d540c00269e056174',
     });
     expect(data.background).toBe(true);
     expect(data.component).toBe('section.section-grid-text');
