ef5980385fedd115389396bcc3d12a8190b070f1
mwises <88902469+mwises@users.noreply.github.com>
2023-10-06 01:21:22 -0300

    adição dos componentes na página

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { mockBase } from '../Base/mock';
 import { Base } from '../Base';
 import * as Styled from './styles';
@@ -5,6 +6,10 @@ import { useEffect, useState } from 'react';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
 import { Loanding } from '../Loading';
+import { GridTwoColumn } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridText } from '../../components/GridText';
+import { GridImage } from '../../components/GridImage';
 
 function Home() {
   const [data, setData] = useState([]);
@@ -34,7 +39,37 @@ function Home() {
     return <Loanding />;
   }
 
-  return <Base {...mockBase} />;
+  const { menu, sections, footerHtml, slug } = data;
+  const { links, text, link, srcImg } = menu;
+
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { componet } = section;
+        const key = `${slug}-${index}`;
+
+        if (componet === 'section.section-two-columns') {
+          return <GridTwoColumn key={key} {...section} />;
+        }
+
+        if (componet === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (componet === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (componet === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 }
 
 export default Home;
