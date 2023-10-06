4899f0c01433dcccf6875b8a8211a31fd3244a09
mwises <88902469+mwises@users.noreply.github.com>
2023-10-06 02:02:16 -0300

    adição de links com React Router Dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './templates/Home';
 import { GlobalStyles } from './styles/global-styles';
 import { theme } from './styles/theme';
@@ -9,8 +10,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Switch>
+          <Route to="*" Component={Home} />
+        </Switch>
+      </BrowserRouter>
       <GlobalStyles />
-      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 );
--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -10,16 +10,19 @@ import { GridTwoColumn } from '../../components/GridTwoColumn';
 import { GridContent } from '../../components/GridContent';
 import { GridText } from '../../components/GridText';
 import { GridImage } from '../../components/GridImage';
+import { useLocation } from 'react-router-dom';
 
 function Home() {
   const [data, setData] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
+    const pathname = location.pathname.replace(/[^a-z0-9-_]/gi, '');
+    const slug = pathname ? pathname : 'landing-page';
+
     const load = async () => {
       try {
-        const data = await fetch(
-          'http://localhost:1337/pages/?slug=landing-page',
-        );
+        const data = await fetch('http://localhost:1337/pages/?slug=' + slug);
         const json = await data.json();
         const pageData = mapData();
         setData(pageData[0]);
