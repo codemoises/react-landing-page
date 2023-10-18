562b473fd5551e304e680a0fcef1c68adf3a93ca
mwises <88902469+mwises@users.noreply.github.com>
2023-10-18 12:33:30 -0300

    Componente MenuLink

--- src/components/MenuLink/index.jsx
+++ src/components/MenuLink/index.jsx
@@ -3,6 +3,7 @@ import * as Styled from './styles';
 
 export const MenuLink = ({ children, link, newTab = false }) => {
   const target = newTab ? '_blank' : '_self';
+
   return (
     <Styled.Container href={link} target={target}>
       {children}
